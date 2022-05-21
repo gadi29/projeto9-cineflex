5eca436951813cade81c3e31249c46a07ff1aa51
gadiel <gad.azevedo@gmail.com>
2022-05-20 22:29:51 -0300

    escolha de sessão

--- src/components/Session.js
+++ src/components/Session.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import styled from "styled-components";
 
 function Day({ weekday, date, showtimes }) {
     const showtimesArray = [...showtimes];
     
     return (
-        <>
-            <h3>{weekday} - {date}</h3>
-            {showtimesArray.map(hour => <button>{hour.name}</button>)}
-        </>
+        <Block>
+            <TextH3>{weekday} - {date}</TextH3>
+            {showtimesArray.map(hour => 
+            <Link to={`/assentos/${hour.id}`}>
+                <Button>{hour.name}</Button>
+            </Link>)}
+        </Block>
     );
 }
 
@@ -30,10 +34,47 @@ function Session() {
     
     return(
         <>
-            <h2>Selecione o horário</h2>
+            <TextH2>Selecione o horário</TextH2>
             {sessions.map(session => <Day weekday={session.weekday} date={session.date} showtimes={session.showtimes}/>)}
         </>
     );
 }
 
 export default Session;
+
+const TextH2 = styled.h2`
+    font-size: 24px;
+    color: #293845;
+    letter-spacing: 0.04em;
+
+    margin: 40px 0;
+
+    text-align: center;
+`;
+
+const Block = styled.div`
+    margin-left: 24px;
+`;
+
+const TextH3 = styled.h3`
+    font-size: 20px;
+    color: #293845;
+`;
+
+const Button = styled.button`
+    border: none;
+    border-radius: 3px;
+    background-color: #E8833A;
+    cursor: pointer;
+
+    padding: 13px 18px;
+    margin-top: 20px;
+    margin-bottom: 25px;
+
+    color: #FFFFFF;
+    font-size: 18px;
+
+    &:last-of-type {
+        margin-left: 9px;
+    }
+`;
